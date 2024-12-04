a014aa58e31b6a9d37940ea77ed937115349bb93
CodecxAb <anuragbabu.868@gmail.com>
2024-12-04 19:15:24 +0530

    landing page added

--- quicar_project/user_auth/views.py
+++ quicar_project/user_auth/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request,'home.html')
 
 def signin(request):
-    return HttpResponse("Hello World auth folder a achi")
+    return render(request,'signin.html')
 
 def signup(request):
-    return HttpResponse("Hello World auth folder a achi")
+   return render(request,'signup.html')
 
 def signout(request):
     return HttpResponse("Hello World auth folder a achi")
