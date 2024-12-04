45cd5fa66ac736cf0dbccae93a3e3f20e2e97863
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-12-04 18:14:01 +0530

    skeleton added

--- quicar_project/quicar_project/settings.py
+++ quicar_project/quicar_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "quicar_app",
+    "manage_cars",
+    "auth",
+    "ai",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = "quicar_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
